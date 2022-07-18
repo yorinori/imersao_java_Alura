8a788d9035d5b57e42e4acca8503a472de6db504
Leandro Yorinori <leandro.yorinori@gmail.com>
2022-07-18 19:41:42 -0300

    fazer uma conexão HTTP e buscar os top 250 filmes

--- Alura-stickers/src/App.java
+++ Alura-stickers/src/App.java
@@ -1,5 +1,23 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // fazer uma conexão HTTP e buscar os top 250 filmes
+        String url = "https://alura-filmes.herokuapp.com/conteudos";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
+
+        // pegar só os dados que interessam (titulo, poster, classificação) [Parsear os dados]
+
+        // exibir e manipular os dados
     }
 }
