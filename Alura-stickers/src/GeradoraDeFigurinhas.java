6a2530c17b440e4f659f34b2028093939cb9e139
Leandro Yorinori <leandro.yorinori@gmail.com>
2022-07-20 01:20:40 -0300

    leitura da imagem

--- Alura-stickers/src/GeradoraDeFigurinhas.java
+++ Alura-stickers/src/GeradoraDeFigurinhas.java
@@ -1,5 +1,23 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
 public class GeradoraDeFigurinhas {
     
-    public void cria();
+
+    public void cria() throws Exception {
+    
+        // leitura da imagem
+        BufferedImage imagemOriginal = ImageIO.read(new File("entrada/filme.jpg"));
+
+        // cria nova imagem em memória com transparência e com tamanho novo
+
+        // copiar a imagem original pra novo imagem (com memória)
+        
+        // escrever uma frase na nova imagem
+
+        // escrever a nova imagem em um arquivo
+    }
 
 }
