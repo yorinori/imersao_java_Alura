e59a82f4872a4276daf411eb928fc82aebbc6c53
Leandro Yorinori <leandro.yorinori@gmail.com>
2022-07-20 02:09:23 -0300

    copiar a imagem original pra novo imagem (com memória) e escrever a nova imagem em um arquivo

--- Alura-stickers/src/GeradoraDeFigurinhas.java
+++ Alura-stickers/src/GeradoraDeFigurinhas.java
@@ -1,5 +1,7 @@
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.nio.file.Files;
 
 import javax.imageio.ImageIO;
 
@@ -9,7 +11,7 @@ public class GeradoraDeFigurinhas {
     public void cria() throws Exception {
     
         // leitura da imagem
-        BufferedImage imagemOriginal = ImageIO.read(new File("entrada/filme.jpg"));
+        BufferedImage imagemOriginal = ImageIO.read(new File("entrada/filme-maior.jpg"));
 
         // cria nova imagem em memória com transparência e com tamanho novo
         int largura = imagemOriginal.getWidth();
@@ -18,10 +20,18 @@ public class GeradoraDeFigurinhas {
         BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
 
         // copiar a imagem original pra novo imagem (com memória)
-        
+        Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
+        graphics.drawImage(imagemOriginal, 0, 0, null);
+
         // escrever uma frase na nova imagem
 
         // escrever a nova imagem em um arquivo
+        ImageIO.write(novaImagem, "png", new File("saida/figurinha.png"));
     }
 
+    public static void main(String[] args) throws Exception {
+        var geradora = new GeradoraDeFigurinhas();
+        geradora.cria();
+
+    }
 }
